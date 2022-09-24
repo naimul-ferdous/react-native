f79bfc415fd49830a8c4094681e6836695155c2f
naimul93 <ferdousnaimul@gmail.com>
2022-09-24 21:31:21 +0600

    005 destructuring props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello konnichiwa</Text>
-      <Button onPress={()=> console.log("Button Pressed")} title="Go to componetns button" />
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to componetns button"
+      />
+      <Button
+        title="Go to list demo"
+        onPress={() => navigation.navigate("List")}
+      />
     </View>
   );
 };
