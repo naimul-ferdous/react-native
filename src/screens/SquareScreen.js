8a0d3a16149c094077ba51a24a84d3a61b8e7a69
naimul93 <ferdousnaimul@gmail.com>
2022-09-27 23:01:51 +0600

    ...

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SquareScreen = () => {
+  return (
+    <View>
+      <Text>SquareScreen</Text>
+    </View>
+  );
+};
+
+export default SquareScreen;
+
+const styles = StyleSheet.create({});
